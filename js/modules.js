b21ae5e6d2088700a2d50b184bf121f94672fb5e
Vilen71 <119299151+Vilen71@users.noreply.github.com>
2023-01-25 01:10:40 +0400

    Add files via upload

---
+++ js/modules.js
@@ -0,0 +1,16 @@
+import svetofor from "./modules/svetofor.js"
+import timer from "./modules/timer.js"
+import outNumber from "./modules/numberRun.js"
+import secundomer from "./modules/secundomer.js"
+import calculator from "./modules/calc.js"
+import invest from "./modules/invest.js"
+import blackjack from "./modules/blackjack.js"
+
+// outNumber()
+svetofor()
+timer(".data", "12 mar 2023 13:45")
+secundomer()
+invest()
+calculator()
+blackjack()
+// export { svetofor, timer, outNumber, secundomer, calculator, invest, blackjack }
